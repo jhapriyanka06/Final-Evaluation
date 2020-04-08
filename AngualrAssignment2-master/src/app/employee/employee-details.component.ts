d6eac0ad7962f33c2e582c3af899483697c7bf03
jhapriyanka06 <61044611+jhapriyanka06@users.noreply.github.com>
2020-04-08 18:23:23 +0530

    updated files

--- AngualrAssignment2-master/src/app/employee/employee-details.component.ts
+++ AngualrAssignment2-master/src/app/employee/employee-details.component.ts
@@ -3,6 +3,8 @@ import { EmployeeService } from '../shared/employee.service';
 import { Employee } from '../shared/employee.model';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { EmployeeLeaveMapping } from '../shared/apply-leave.model';
+import { ApplyLeaveService } from '../shared/apply-leave.service';
 
 @Component({
   selector:'employee-list',
@@ -12,6 +14,9 @@ export class EmployeeDetailsComponent implements OnInit{
 public pageTitle="Employee Details";
 employeedetails:Employee[]=[];
 employee:Employee[];
+leavemappingdetails:EmployeeLeaveMapping[]=[];
+leavemapping:EmployeeLeaveMapping[];
+empleavmap:EmployeeLeaveMapping;
 _listFilter:string;
 get listFilter():string{
     return this._listFilter;
@@ -28,7 +33,7 @@ performFilter(filterBy: string): Employee[]{
 }
   constructor(private service:EmployeeService,
     private http:HttpClient,
-    private router:Router){}
+    private router:Router,private empleaveervice:ApplyLeaveService){}
 
   ngOnInit():void {
     this.service.getEmployees().subscribe({
@@ -46,12 +51,38 @@ performFilter(filterBy: string): Employee[]{
       res =>{
 
         this.onSaveComplete();
+        this.DeletingFromLeave(id);
       },
       err=>{
         console.log(err);
       }
     )
   }
+  }
+  DeletingFromLeave(id:number){
+    this.empleaveervice.getLeaves().subscribe({
+      next: leave => {
+        this.leavemapping = leave;
+        this.leavemappingdetails = this.leavemapping;
+        this.leavemappingdetails = this.leavemapping.filter(l => l.employeeid===id);
+        this.DeleteE();
+        },
+     });
+
+  }
+  ID:number;
+  DeleteE(){
+    for(var i=0;i<this.leavemappingdetails.length;i++){
+      this.ID=this.leavemappingdetails[i].id;
+      this.empleaveervice.deleteEmployee(this.ID).subscribe(
+        res =>{
+        this.onSaveComplete();
+        },
+        err=>{
+          console.log(err);
+        }
+      )
+     }
   }
    onSaveComplete(): void {
     this.service.getEmployees().subscribe({
